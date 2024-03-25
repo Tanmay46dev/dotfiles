6a0f1bdf15dff91d7a8a4873574a12ecae30b4ae
Tanmay46dev <86778333+Tanmay46dev@users.noreply.github.com>
2024-03-26 00:51:37 +0530

    Add files via upload

---
+++ qtile/colors.py
@@ -0,0 +1,13 @@
+GRUVBOX = {
+	"bg": "#282828",
+    "fg": "#ebdbb2",
+    "gray": "a89984",
+    "dark-gray-0": "928374",
+	"red": "#b85651",
+    "orange": "#bd6f3e",
+    "yellow": "#c18f41",
+    "green": "#8f9a52",
+    "aqua": "#72966c",
+    "blue": "#68948a",
+    "purple": "#ab6c7d",
+}
